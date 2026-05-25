e800be9f38e1195f2a9ecca773a9a5e177cdaef4
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-05-25 11:55:45 -0300

    Create constantes.js with variable type demonstrations
    Define variables of different types and log their values and types.

---
+++ UC2/Aulas/Exercicios/constantes.js
@@ -0,0 +1,27 @@
+let x = 3
+const pi = 3.14
+
+console.log(`Temos ${x} e ${pi}. \n`)
+
+x = 5 // Inteiro (int)          | number
+console.log(x, typeof(x))
+x = 1.2 // Float                | number
+console.log(x, typeof(x))
+x = "Nome!" // String           | string
+console.log(x, typeof(x))
+x = true // Boolean             | bool
+console.log(x, typeof(x))
+x = false // Boolean            | bool
+console.log(x, typeof(x))
+x = [1, 2, "String"] // Array   | object
+console.log(x, typeof(x))
+
+
+
+/* 
+    int = números inteiros;
+    float = números quebrados;
+    bool = verdadeiro ou falso (true or false)
+
+    Os números quebrados sempre devem ser utilizados com ponto (.), a única função da vírgula (,) é separar as variáveis (nos arrays);
+*/
