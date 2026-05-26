06ede84f3633c998ec3fa33df2a794eafdcc5c48
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-05-26 11:19:33 -0300

    Add main.js with activities and personal info

---
+++ UC2/Exercicios/Tarefas/ex001/main.js
@@ -0,0 +1,29 @@
+// Atividade do dia 26/05/2026.
+
+console.log("Atividade 1 \n")
+
+let nome = "Valdir"
+let idade = 18
+let cidade = "São Leopoldo (infelizmente)"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e vivo em ${cidade}.`)
+
+console.log("\n Atividade 2 \n")
+
+const curso = "Estou estudando Técnico em Desenvolvimento de Sistemas"
+
+console.log(curso)
+
+console.log("\n Atividade extra \n")
+
+let comidaFav = "Massa com salsicha"
+let jogoFav = "Hollow Knight?????"
+let lang = "JavaScript"
+
+console.log("Olhe o meu perfil! \n")
+console.log("Meu nome é: " + nome)
+console.log("EU tenho " + idade + " anos.")
+console.log("Minha cidade atual é " + cidade)
+console.log("Minha comida favorita é " + comidaFav + " (se quiser trazer algum dia)")
+console.log("Meu jogo favorito talvez seja " + jogoFav)
+console.log("Linguagem que estou aprendendo: " + lang)
