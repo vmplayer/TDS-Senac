f6edce099a751e38258e321f7a771656a3adf9a3
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-06-02 09:59:14 -0300

    Add example.js demonstrating comparison operators

---
+++ UC2/Exercicios/Exemplos/02-06/exemplo.js
@@ -0,0 +1,19 @@
+let idade = 18
+
+// Operador de maior que [>]
+console.log(idade > 15) // True
+
+// Operador de menor que [<]
+console.log(idade < 20) // True
+
+// Operador de maior ou igual [>=]
+console.log(idade >= 18) // True
+
+// Operador de menor ou igual [<=]
+console.log(idade <= 18) // True
+
+// Operador de igual [==]
+console.log(idade == 20) // False
+
+// Operador de diferente [!=]
+console.log(idade != 20) // True
