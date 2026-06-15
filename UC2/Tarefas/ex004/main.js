d69cc10b2430e7e46003dfe6e230e9a8401ff8df
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-06-15 11:07:42 -0300

    Refactor fruit selection and apply discount logic

--- UC2/Tarefas/ex004/main.js
+++ UC2/Tarefas/ex004/main.js
@@ -2,25 +2,21 @@ const rl = require('readline-sync')
 
 let final = false
 const frutas = [
-    { id: 1, nome: "maca", tempo: 2, valor: 0.72 },
-    { id: 2, nome: "banana", tempo: 1, valor: 0.86 },
-    { id: 3, nome: "pera", tempo: 4, valor: 0.52 },
-    { id: 4, nome: "maracuja", tempo: 2, valor: 0.63 }
+    { id: 0, nome: "maca", tempo: 2, valor: 0.72 },
+    { id: 1, nome: "banana", tempo: 1, valor: 0.86 },
+    { id: 2, nome: "pera", tempo: 4, valor: 0.52 },
+    { id: 3, nome: "maracuja", tempo: 2, valor: 0.63 }
 ]
 
 let resultado
 
-// let sim = frutas.includes("maca")
+// let sim = frutas.includes("nome")
 // console.log(`AQUI >>>>> ${sim}`)
 
-let valor = frutas.push(resultado)
+let valor = 0
 let carrinho = []
 
-// if (frutas.maca.tempo >= 3 || frutas.banana.tempo >= 3 || frutas.pera.tempo >= 3 || frutas.maracuja.tempo >= 3) {
-    // Aplicar 30% de desconto se a fruta estiver na prateleira há mais de 3 dias
-// }
-
-const opcoes = ["Maçã", "Banana", "Pera", "Maracujá"]
+const opcoes = ["Maca", "Banana", "Pera", "Maracuja"]
 let response
 while (!final) {
     response = rl.keyInSelect(opcoes, 'O que voce deseja comprar?')
@@ -28,10 +24,22 @@ while (!final) {
     if (response === -1) {
         final = true
         console.log("Compra finalizada!")
-        console.log(`O valor total da compra ficou em: R\$${valor}`)
+        console.log(`O valor total da compra ficou em: R\$${valor.toFixed(2)}`)
+
     } else {
-        resultado = frutas.find(f => f.id === 1)
-        console.log(`Você colocou ${response} na cesta.`)
-        console.log(`Seu carrinho está avaliado em R\$${valor}`)
+        resultado = frutas.find(f => f.id === response)
+        tempo = frutas.find(f => f.tempo === response)
+        let valorFruta = resultado.valor
+        
+        // Aplica 30% de desconto se tiver mais de 3 dias de prateleira.
+        if (resultado.tempo >= 3) {
+            console.log("[PROMOÇÃO] Como a sua fruta está há mais de 3 dias na prateleira, estamos aplicando 30% de desconto!")
+            valorFruta *= 0.7
+        }
+
+        valor = valor + valorFruta
+
+        console.log(`Você adicionou ${resultado.nome} na cesta.`)
+        console.log(`Seu carrinho está avaliado em R\$${valor.toFixed(2)}.`)
     }
 }
