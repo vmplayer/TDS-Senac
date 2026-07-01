12b4c3def669374b5ddac0eeeea0115051c4d531
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-01 12:01:20 -0300

    Add interactive menu in menu.js
    Implement a menu with options for greetings and exit.

---
+++ UC2/Exercicios/01-07/menu.js
@@ -0,0 +1,39 @@
+const rl = require('readline-sync')
+
+let opcao
+
+while (opcao != 0) {
+    console.log(`
+        --- MENU ---
+        [1] Olá
+        [2] Tchau!
+        [3] Rodorlfo
+        [0] Sair        
+    `)
+}
+
+opcao = rl.questionInt('Escolha sua opcao: ')
+
+switch (opcao) {
+    case 1:
+        console.log('Olá!')
+        break
+
+    case 2:
+        console.log('Tchau!')
+        break
+
+    case 3: 
+        console.log('Rodorlfo.')
+        break
+
+    case 0:
+        console.log('Saindo...')
+        break
+
+    default:
+        console.log('Opção inválida. Tente novamente!')
+
+}
+
+// Tem algo errado que ta dando loop infinito, olha isso ai depois eu do futuro.
