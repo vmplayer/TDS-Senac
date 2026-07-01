c364f328182ff7f9cd022cf850207e6173f14209
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-01 12:01:40 -0300

    Add for loop to display numbers and calculate sum
    This script displays numbers from 10 to 0 and calculates the sum of numbers from 1 to 10.

---
+++ UC2/Exercicios/01-07/for.js
@@ -0,0 +1,16 @@
+
+// Exibir números de 1 a 10
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
+}
+
+// Exibir o valor da soma de 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+let soma = 0
+
+for (let i = 1; i <= 10; i++) {
+    soma += i
+}
+
+console.log(soma)
