7441240260fc56ce88e4b7fd8c46d77f5c1c6108
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-06 11:52:00 -0300

    Add quantity to fruit objects and update cart logic

--- UC2/Projetos/PJ001 - Projeto Fruta/main.js
+++ UC2/Projetos/PJ001 - Projeto Fruta/main.js
@@ -1,48 +1,73 @@
 const rl = require('readline-sync')
 
-let final = false
 const frutas = [
-    { id: 0, nome: "maca", tempo: 2, valor: 0.72 },
-    { id: 1, nome: "banana", tempo: 1, valor: 0.86 },
-    { id: 2, nome: "pera", tempo: 4, valor: 0.52 },
-    { id: 3, nome: "maracuja", tempo: 2, valor: 0.63 }
+    { id: 0, nome: "maca", tempo: 2, valor: 0.72, quantidade: 134 },
+    { id: 1, nome: "banana", tempo: 1, valor: 0.86, quantidade: 97 },
+    { id: 2, nome: "pera", tempo: 4, valor: 0.52, quantidade: 40 },
+    { id: 3, nome: "maracuja", tempo: 2, valor: 0.63, quantidade: 46 }
 ]
 
+let final = false
+let response
 let resultado
-
-// let sim = frutas.includes("nome")
-// console.log(`AQUI >>>>> ${sim}`)
-
 let valor = 0
 
-// Gerenciamento de estoque e carrinho
+// Funções e gerenciamento do carrinho
 
-let estoque = []
 let carrinho = []
-let items = []
+
+function addItem(fruta, valorAtual) {
+    fruta.quantidade--
+
+    carrinho.push(fruta)
+    valor += valorAtual
+
+}
+
+function viewCart() {
+    if (carrinho.lenght === 0) {
+        console.log("O seu carrinho está vazio.")
+    } else {
+        for (let i = 0; i < carrinho.length; i++) {
+            const item = carrinho[i]
+            console.log(item.nome)
+        }
+    }
+
+    console.log(`\nValor final: R\$${valor.toFixed(2)}.`)
+}
+
+// Interface
 
 console.clear()
 
-const opcoes = ["Maca", "Banana", "Pera", "Maracuja", "Carrinho"]
-let response
+const opcoes = ["- Maca", "- Banana", "- Pera", "- Maracuja", "- Carrinho"]
 while (!final) {
-    response = rl.keyInSelect(opcoes, 'O que voce deseja comprar? ', { cancel: 'Finalizar a compra' })
-
     console.log("=== VENDA DE FRUTAS ===")
     console.log("Venha, venha! Nossas frutas são de ótima qualidade, compre conosco e depois compre de novo!")
+
+    response = rl.keyInSelect(opcoes, 'O que voce deseja comprar? ', { cancel: '- Finalizar a compra' })
 
     if (response === -1) {
         final = true
 
         console.clear()
+        console.log("=== VENDA DE FRUTAS ===")
+        console.log("Venha, venha! Nossas frutas são de ótima qualidade, compre conosco e depois compre de novo!")
+        console.log()
         console.log("Compra finalizada!")
         console.log(`O valor total da compra ficou em: R\$${valor.toFixed(2)}.`)
 
-    } else if (items === 0) {
-
+    } else if (frutas.quantidade === 0) {
+        resultado = frutas.find(f => f.quantidade === response)
+        
+        console.log("O item que você deseja está fora de estoque.")
     } else if (response === 4) {
         console.log("Carrinho Acessado!")
-        final = true
+
+        viewCart()
+
+        volt = rl.keyIn('[0] - Fechar o carrinho', { limit: '0' })
     } else {
         resultado = frutas.find(f => f.id === response)
         tempo = frutas.find(f => f.tempo === response)
@@ -53,12 +78,17 @@ while (!final) {
         // Aplica 30% de desconto se tiver mais de 3 dias de prateleira.
         if (resultado.tempo >= 3) {
             console.log("[PROMOÇÃO] Como a sua fruta está há mais de 3 dias na prateleira, estamos aplicando 30% de desconto!")
+            console.log("Valor sem desconto: R$" + valorFruta.toFixed(2))
             valorFruta *= 0.7
+            console.log("Valor com desconto: R$" + valorFruta.toFixed(2))
+        } else {
+
         }
 
-        valor = valor + valorFruta
+        addItem(resultado, valorFruta)
 
         console.log(`Você adicionou ${resultado.nome} na cesta.`)
         console.log(`Seu carrinho está avaliado em R\$${valor.toFixed(2)}.`)
+        console.log()
     }
 }
