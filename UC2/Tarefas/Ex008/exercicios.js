ec3fb6a1db97f1b51e2aa5667389193fad96cf67
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-09 10:15:40 -0300

    Uncomment code for exercises in exercicios.js

--- UC2/Tarefas/Ex008/exercicios.js
+++ UC2/Tarefas/Ex008/exercicios.js
@@ -1,73 +1,73 @@
 const rls = require('readline-sync')
 
-// console.log("=== EXERCICIO 1 ===\n")
+console.log("=== EXERCICIO 1 ===\n")
 
-// const nomes = ["Anakin Skywalker", "Obi-wan", "Padmé Amidala", "Boba Fett", "Mandaloriano"]
+const nomes = ["Anakin Skywalker", "Obi-wan", "Padmé Amidala", "Boba Fett", "Mandaloriano"]
 
-// for (let nome of nomes) {
-//     console.log(nome)
-// }
+for (let nome of nomes) {
+    console.log(nome)
+}
 
-// console.log("\n=== EXERCICIO 2 ===\n")
+console.log("\n=== EXERCICIO 2 ===\n")
 
-// for (let i in nomes) {
-//     console.log(`${i} - ${nomes[i]}`)
-// }
+for (let i in nomes) {
+    console.log(`${i} - ${nomes[i]}`)
+}
 
-// console.log("\n=== EXERCICIO 3 ===\n")
+console.log("\n=== EXERCICIO 3 ===\n")
 
-// let lista = []
+let lista = []
 
-// console.log(`Lista atual: ${lista}\n`)
+console.log(`Lista atual: ${lista}\n`)
 
 
-// while (lista.length < 5) {
-//     let item = rls.question("Qual item voce quer adicionar? ")
-//     lista.push(item)
-// }
-// console.log()
+while (lista.length < 5) {
+    let item = rls.question("Qual item voce quer adicionar? ")
+    lista.push(item)
+}
+console.log()
 
-// lista.forEach((item, i) => {
-//     console.log(`[${i + 1}] - ${item}`)
-// })
+lista.forEach((item, i) => {
+    console.log(`[${i + 1}] - ${item}`)
+})
 
-// console.log("\n=== EXERCICIO 4 ===\n")
+console.log("\n=== EXERCICIO 4 ===\n")
 
-// const frutas = ["Maca", "Banana", "Maca do Notch", "Jogan", "Frutas Douradas de Lothlórien"]
+const frutas = ["Maca", "Banana", "Maca do Notch", "Jogan", "Frutas Douradas de Lothlórien"]
 
-// console.log("Frutas disponíveis:")
-// for (let fruta of frutas) {
-//     console.log(fruta)
-// }
+console.log("Frutas disponíveis:")
+for (let fruta of frutas) {
+    console.log(fruta)
+}
 
-// let search = rls.question("Qual fruta voce quer pesquisar? ")
-// let result = frutas.includes(search)
+let search = rls.question("Qual fruta voce quer pesquisar? ")
+let result = frutas.includes(search)
 
-// if (!result) {
-//     console.log("A fruta não está disponível.")
-// } else {
-//     console.log("A fruta desejada não foi encontrada.")
-// }
+if (!result) {
+    console.log("A fruta não está disponível.")
+} else {
+    console.log("A fruta desejada foi encontrada.")
+}
 
-// console.log("\n=== EXERCICIO 5 ===\n")
+console.log("\n=== EXERCICIO 5 ===\n")
 
-// let cidades = ["Pripyat", "Fukushima", "Goiania", "Hiroshima", "Nagasaki"]
+let cidades = ["Pripyat", "Fukushima", "Goiania", "Hiroshima", "Nagasaki"]
 
-// console.log("Cidades do nosso sistema:")
-// console.log(cidades.join(", "))
+console.log("Cidades do nosso sistema:")
+console.log(cidades.join(", "))
 
-// cidades.splice(2, 1)
-// console.log(cidades.join(", "))
+cidades.splice(2, 1)
+console.log(cidades.join(", "))
 
-// console.log("\n=== EXERCICIO 6 ===\n")
+console.log("\n=== EXERCICIO 6 ===\n")
 
-// let langs = ["JavaScript", "Python", "Java", "C#", "PHP"]
+let langs = ["JavaScript", "Python", "Java", "C#", "PHP"]
 
-// console.log(langs.join(" | "))
+console.log(langs.join(" | "))
 
-// console.log("[ALERTA] REVISE AGORA.")
-// console.log("Pressione [ESPAÇO] para prosseguir para o desafio.")
-// rls.keyInPause('', { limit: ' ' })
+console.log("\n[ALERTA] REVISE AGORA.")
+console.log("Pressione [ESPAÇO] para prosseguir para o desafio.")
+rls.keyInPause('', { limit: ' ' })
 
 console.clear()
 
@@ -86,9 +86,26 @@ produtos.forEach((item, i) => {
 
 let qualProduto = rls.question("Qual produto voce deseja remover? ")
 let resultado = produtos.includes(qualProduto)
+let num = Number(qualProduto)
+let resultadoNum = num > 0 && num <= produtos.length
 
 if (resultado) {
-    console.log("Sim")
+    let removedItem = produtos.indexOf(qualProduto)
+    produtos.splice(removedItem, 1)
+
+    // Usei um forEach por preferência própria mesmo
+    console.log("Carrinho atualizado: ")
+    produtos.forEach((item, i) => {
+        console.log(`[${i + 1}] - ${item}`)
+    })
+} else if (resultadoNum) {
+    let removedItem = num - 1
+    produtos.splice(removedItem, 1)
+
+    console.log("Carrinho atualizado: ")
+    produtos.forEach((item, i) => {
+        console.log(`[${i + 1}] - ${item}`)
+    })
 } else {
-    console.log("nao")
+    console.log("Não encontramos o produto que selecionado.")
 }
