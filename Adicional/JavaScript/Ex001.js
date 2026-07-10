8cdbd3cdf3f815cf38698a9cdc95e5145f96a873
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-10 11:37:56 -0300

    Add Ex001.js with multiple programming exercises

---
+++ Adicional/JavaScript/Ex001.js
@@ -0,0 +1,88 @@
+const rls = require('readline-sync')
+
+// console.log("=== EXERCICIO 1 ===\n")
+
+// let linguagens = [
+//     "JavaScript",
+//     "Python",
+//     "Java", 
+//     "C#",
+//     "C++",
+//     "C",
+//     "PHP",
+//     "MySQL",
+//     "SQLite"
+// ]
+
+// let qualLinguagem = rls.question("Qual linguagem de programacao voce deseja? ")
+
+// let achou = linguagens.includes(qualLinguagem)
+
+// if (achou) {
+//     console.log("Linguagem encontrada!")
+// } else {
+//     console.log("Linguagem não encontrada!")
+// }
+
+// console.log("\n=== EXERCICIO 2 ===\n")
+
+// let produtos = [
+//     "Mouse",
+//     "Teclado",
+//     "Monitor",
+//     "Webcam",
+//     "Notebook"
+// ]
+
+// for (let produto of produtos) {
+//     console.log(produto)
+// }
+
+// let search = rls.question("\nQual produto voce quer encontrar? ")
+// let searchIndex = produtos.indexOf(search)
+
+// if (searchIndex !== -1) {
+//     console.log(`O produto foi encontrado na posição ${searchIndex + 1}.`)
+// } else {
+//     console.log("O produto não foi encontrado.")
+// }
+
+// console.log("\n=== EXERCICIO 3 ===\n")
+
+// let cores = [
+//     "Azul",
+//     "Verde", 
+//     "Vermelho", 
+//     "Amarelo"
+// ]
+
+// for (let cor of cores) {
+//     console.log(cor)
+// }
+
+// cores.splice(2, 1, "Roxo")
+// console.log("\nNovas cores:")
+
+// for (let cor of cores) {
+//     console.log(cor)
+// }
+
+// console.log("\n=== EXERCICIO 4 ===\n")
+
+// let numeros = [
+//     "10",
+//     "20",
+//     "30",
+//     "40",
+//     "50"
+// ]
+
+// console.log("Array antes: ")
+// console.log(numeros.join(", "))
+// numeros.pop()
+
+// console.log("\nArray depois: ")
+// console.log(numeros.join(", "))
+
+console.log("\n=== EXERCICIO 5 ===\n")
+
