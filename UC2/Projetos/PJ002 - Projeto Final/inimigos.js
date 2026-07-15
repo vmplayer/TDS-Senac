e5eda31bc692b2bed5cde9dcb4fd522a0032e09e
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-15 11:56:14 -0300

    Add enemies.js with enemy definitions

---
+++ UC2/Projetos/PJ002 - Projeto Final/inimigos.js
@@ -0,0 +1,8 @@
+const { player, classes } = require('./player.js')
+
+let enemies = [
+    { nome: "Goblin", vida: 30, ataque: 8, drop: "veneno" },
+    { nome: "Orc", vida: 50, ataque: 12, drop: "faca velha" },
+    { nome: "Dragão", vida: 200, ataque: 20, drop: "end" }
+]
+
