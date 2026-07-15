7593caeb1ffc5491b059d05e47c889aa55e08eff
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-15 11:55:42 -0300

    Refactor character creation and menu system

--- UC2/Projetos/PJ002 - Projeto Final/main.js
+++ UC2/Projetos/PJ002 - Projeto Final/main.js
@@ -1,7 +1,16 @@
 const rls = require('readline-sync')
 const { player, classes } = require('./player')
+const { inventory, weapons, misc } = require('./inventario.js')
 
-console.log('=== CRIAR PERSONAGEM ===')
+console.clear()
+console.log('\n=== CRIAR PERSONAGEM ===\n')
+
+// Sistema para dar o nome da máscara
+
+player.nome = rls.question('Digite o seu nome: ')
+console.log(`Acorde, ${player.nome}.`)
+
+// Sistema de seleção de classe
 
 while (player.classe == "") {
     console.clear()
@@ -11,31 +20,99 @@ while (player.classe == "") {
         [1] - Guerreiro
         [2] - Mago
         [3] - Arqueiro
-        `)
+    `)
 
-    player.classe = rls.questionInt('Se eu pudesse ser um herói, eu seria...\n')
+    console.log('Se eu pudesse ser um herói, eu seria...')
+    player.classe = rls.questionInt()
 
     switch (player.classe) {
-        case 0:
+        case 1:
             player.classe = classes[0].nome
             player.hp = classes[0].hp
+            player.maxhp = classes[0].hp
             player.atk = classes[0].atk
             break
-        
-        case 1:
+
+        case 2:
             player.classe = classes[1].nome
             player.hp = classes[1].hp
+            player.maxhp = classes[1].hp
             player.atk = classes[1].atk
             break   
         
-        case 2:
+        case 3:
             player.classe = classes[2].nome
             player.hp = classes[2].hp
+            player.maxhp = classes[2].hp
             player.atk = classes[2].atk
             break
-
+    
         default:
-            console.log("Coloque um valor válido")
+            player.classe = ""
     }
 }
-console.log(player['classe'])
+
+console.clear()
+console.log(`\nEu acho que eu iria querer ser um ${player.classe}...`)
+let menu = true
+// Interface do usuário (UI)
+
+while (menu) {
+    console.log(`
+        [1] - EXPLORAR
+        [2] - VER STATUS
+        [3] - INVENTÁRIO
+        [4] - DESCANSAR
+        [5] - SAIR
+    `)
+
+    let option = rls.keyIn('', {limit: '12345'})
+    switch (option) {
+        case 1:
+            console.log("Explorando...")
+        
+        case 2:
+            showStats()
+
+        case 3:
+            console.log("Vendo inventário...")
+
+        case 4:
+            console.log("Descansando...")
+
+        case 5:
+            console.log("Saindo...")
+            menu = false
+    }
+}
+
+
+
+
+// console.log(`NOME: ${player.nome}, O ${player.classe}    VIDA: ${player.hp}/${player.maxhp}`.toUpperCase())
+
+// FUNÇÕES
+
+function showStats() {
+    console.clear()
+    console.log(`
+        === STATUS DO JOGADOR ===
+        
+          NOME:  ${player.nome}
+        CLASSE:  ${player.classe}
+          VIDA:  ${player.hp}/${player.maxhp}
+        ATAQUE:  ${player.atk}
+         NÍVEL:  ${player.lvl} (${player.exp})
+         ITENS:  ${inventory.itemsOn}/${inventory.maxItemsOn}
+    `)
+}
+
+function gameClear() {
+    console.clear()
+    console.log("Concluiu.")
+}
+
+function gameOver() {
+    console.clear()
+    console.log("Fim de jogo.")
+}
