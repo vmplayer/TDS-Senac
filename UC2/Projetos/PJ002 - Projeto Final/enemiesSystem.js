a129deaf598041f85da0064d615e49c93b693f8f
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-22 11:54:15 -0300

    Add new enemies to enemiesSystem.js

--- UC2/Projetos/PJ002 - Projeto Final/enemiesSystem.js
+++ UC2/Projetos/PJ002 - Projeto Final/enemiesSystem.js
@@ -1,9 +1,9 @@
 const { player, classes } = require('./playerSystem.js')
 
 let enemies = [
-    { nome: "Goblin", hp: 30, atk: 8, def: 5, drop: "Tempo" },
-    { nome: "Orc", hp: 50, atk: 12, def: 8, drop: "Culpa" },
-    { nome: "Dragão", hp: 200, atk: 20, def: 12, drop: "Entorpecente" }
+    { nome: "Sombra", type: "Sombra", hp: 30, atk: 8, def: 5, drop: "Tempo" },
+    { nome: "O Mascarado", type: "O Mascarado", hp: 50, atk: 12, def: 8, drop: "Culpa" },
+    { nome: "O Criador", type: "O Criador", hp: 200, atk: 20, def: 12, drop: "Entorpecente" }
 ]
 
 let activeEnemies = []
