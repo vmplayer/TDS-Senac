bbc889e288ca1759801dc9ddcae698d42816ec62
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-07-22 23:14:23 -0300

    Update player level to 1 and add new attributes

--- UC2/Projetos/PJ002 - Projeto Final/playerSystem.js
+++ UC2/Projetos/PJ002 - Projeto Final/playerSystem.js
@@ -5,13 +5,17 @@ let player = {
     maxhp: "",
     atk: "",
     def: "",
-    lvl: 0,
+    lvl: 1,
     blood: 100,
     enemiesDefeated: 0,
     enemiesDefType: [],
     restTimes: 0,
-    hasEscaped: 0
+    hasEscaped: 0,
+    tempAtkBonus: 0,
+    bonusRounds: 0
 }
+
+// Classes contendo as informações dos jogadores
 
 let classes = [
     { id: 1, nome: "Guerreiro", hp: 120, atk: 18, def: 12 },
